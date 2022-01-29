a2ee9548872333b821134971cf4c56c1a9e3a19b
Ben <benchuan0614@gmail.com>
2022-01-29 19:38:05 +0800

    調整LazyLoad

--- components/FixedJoin/index.tsx
+++ components/FixedJoin/index.tsx
@@ -6,13 +6,10 @@ import Image from "next/image";
 function FixedJoin() {
   return (
     <>
-      {/* 添加lazyload 隱藏住runtime */}
-      <LazyLoad>
-        <FixedJoinWrap>
-          {/* 這裡用bg做 但也可以用Image*/}
-          {/* <Image src="/images/join.png" alt="" height={130} width={130}></Image> */}
-        </FixedJoinWrap>
-      </LazyLoad>
+      <FixedJoinWrap>
+        {/* 這裡用bg做 但也可以用Image*/}
+        {/* <Image src="/images/join.png" alt="" height={130} width={130}></Image> */}
+      </FixedJoinWrap>
     </>
   );
 }
